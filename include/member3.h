d6efbd927129fbf456ec05aa1fb924f4818b3152
GoliAdr <adrian.golis@student.nhlstenden.com>
2025-10-09 20:18:55 +0200

    Member2: menu wiring (0 blink, 1 sum, 2 multiply)

---
+++ include/member3.h
@@ -0,0 +1,2 @@
+#pragma once
+void member3_sum();
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,31 @@
 #include <Arduino.h>
+#include "member1.h"
+#include "member3.h"
+#include "member4.h"
 
-// put function declarations here:
-int myFunction(int, int);
+static void printMenu() {
+  Serial.println("\n0=Blink  1=Sum  2=Multiply");
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  printMenu();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  if (!Serial.available()) return;
+  char c = Serial.read();
+  if (c=='\r' || c=='\n') return;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  switch (c) {
+    case '0': member1_blink();      break;    //Blinki Blink
+    case '1': member3_sum();        break;    // Member 3 handles reading a,b + prints result
+    case '2': member4_multiply();   break;    // Member 4 handles reading a,b + prints result
+    default:  Serial.println("Invalid choice");
+  }
+
+  // clear rest of the line and reprint menu
+  while (Serial.available()) Serial.read();
+  printMenu();
 }
